922a619045363c169c74fc5bf40d34cccec7bbff
mostafa <mostafa.payam.developer45@gmail.com>
2024-07-20 04:46:11 +0330

    hello1111

---
+++ src/main/java/com/mosta/hellowordll/HellowordllApplication.java
@@ -0,0 +1,13 @@
+package com.mosta.hellowordll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowordllApplication.class, args);
+    }
+
+}
